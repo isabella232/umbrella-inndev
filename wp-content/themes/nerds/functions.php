bef5bd9993ad710f45c14d37b0d9f40387f32082
adam schweigert <aschweigert@gmail.com>
2014-05-22 15:17:13 -0400

    stylin

--- wp-content/themes/nerds/functions.php
+++ wp-content/themes/nerds/functions.php
@@ -8,3 +8,10 @@ function override_largo_core_js() {
 	wp_enqueue_script('nerdCore', get_stylesheet_directory_uri() . '/js/nerdCore.js', array( 'jquery' ), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'override_largo_core_js', 20 );
+
+function inn_typekit() { ?>
+	<script type="text/javascript" src="//use.typekit.net/mmy6iwx.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php
+}
+add_action( 'wp_head', 'inn_typekit' );
