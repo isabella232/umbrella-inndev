d019dfea237cecc496d09717b26a8036d21abafd
adam schweigert <aschweigert@gmail.com>
2014-05-23 09:00:35 -0400

    removing the subtitle metabox for now

--- wp-content/themes/nerds/inc/metaboxes.php
+++ wp-content/themes/nerds/inc/metaboxes.php
@@ -1,27 +1,5 @@
 <?php
 /**
- * Add a subtitle metabox
- */
-largo_add_meta_box(
-	'subtitle',
-	'Subtitle',
-	'subtitle_meta_box_display',
-	'post',
-	'normal',
-	'core'
-);
-function subtitle_meta_box_display() {
-	global $post;
-	$values = get_post_custom( $post->ID );
-	wp_nonce_field( 'largo_meta_box_nonce', 'meta_box_nonce' );
-	?>
-	<label for="subtitle"><?php _e('Subtitle', 'largo'); ?></label>
-	<textarea name="subtitle" id="subtitle" class="widefat" rows="2" cols="20"><?php if ( isset ( $values['subtitle'] ) ) echo $values['subtitle'][0]; ?></textarea>
-	<?php
-	largo_register_meta_input( 'subtitle' );
-}
-
-/**
  * Add featured image display metabox
  * (allows author to override automatic display of featured image as the "cover image" on single post page)
  */
