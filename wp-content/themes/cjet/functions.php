8a712d814bd8b24ef1808c7725d381fd0cced037
cornershopcreative <hello@cornershopcreative.com>
2014-05-29 16:55:07 +0000

    Logic and styling for guide pages

---
+++ wp-content/themes/cjet/functions.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * CJET theme stuff
+ *
+ * Most of the brains live in Largo, of course
+ */
+
+
+/**
+ * Formats an attachment link for inclusion on Guide pages, with icon, filesize & type, etc.
+ *
+ * @access public
+ * @param integer $attachment_id
+ * @return void
+ */
+function cjet_format_attachment_link( $attachment_id ) {
+
+	$extras = cjet_attachment_extras( $attachment_id );
+
+	switch ( $extras['human_type'] ) {
+		case "Image":
+			$human_type_css_class = "picture";
+			break;
+		case "Video":
+			$human_type_css_class = "play";
+			break;
+		case "Excel":
+			$human_type_css_class = "table";
+			break;
+		case "Word":
+			$human_type_css_class = "doc-text";
+			break;
+		case "PDF":
+			$human_type_css_class = "doc-text-inv";
+			break;
+		default:
+			$human_type_css_class = "download";
+	}
+
+	$details = array();
+	if ( !empty($extras['human_type'] )) $details[] = $extras['human_type'];
+	if ( !empty($extras['filesize'] )) $details[] = size_format($extras['filesize']);
+
+	$output = sprintf('<a href="%s" title="%s"><i class="%s"></i> %s <span class="attachment-meta">(%s)</span></a>',
+		wp_get_attachment_url( $attachment_id ),
+		__('Permalink to', 'cjet') . ' ' . esc_attr( get_the_title( $attachment_id ) ),
+		'icon-' . $human_type_css_class,
+		get_the_title( $attachment_id ),
+		implode(", ", $details)
+	);
+	return $output;
+}
+
+
+/**
+ * Gets some extra information (simplified filetype and file size) about the provided attachment ID and returns it as an array.
+ *
+ * @access public
+ * @param integer $attachment_id
+ * @return array
+ */
+function cjet_attachment_extras( $attachment_id ) {
+
+	$human_type = "";
+	$mime = get_post_mime_type( $attachment_id );
+	switch( $mime ) {
+		case "image/jpeg":
+		case "image/jpg":
+		case "image/png":
+		case "image/gif":
+			$human_type = "Image";
+			break;
+		case "video/mpeg":
+		case "video/mp4":
+		case "video/quicktime":
+			$human_type = "Video";
+			break;
+		case "application/vnd.ms-excel":
+		case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+		case "text/csv":
+			$human_type = "Excel";
+			break;
+		case "application/msword":
+		case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+			$human_type = "Word";
+			break;
+		case "application/pdf":
+			$human_type = "PDF";
+			break;
+		//case "application/vnd.ms-powerpoint"
+	}
+
+	return array(
+		'human_type' => $human_type,
+		'filesize' => @filesize( get_attached_file( $attachment_id )),
+	);
+
+}
+
+/**
+ * Load up stuff we need
+ */
+function cjet_enqueue() {
+	//Get our JS file
+	wp_enqueue_script( 'cjet-javascript', get_stylesheet_directory_uri() . '/cjet.js' );
+}
+add_action( 'wp_enqueue_scripts', 'cjet_enqueue' );
---
+++ wp-content/themes/cjet/guide.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Template Name: Guide Page
+ */
+get_header();
+$top_page = FALSE;
+?>
+
+<div id="content" class="span12 guide-page" role="main">
+	<?php
+		while ( have_posts() ) : the_post();
+
+			// get the ID of the main page for a given guide
+			$this_page_id = $post->ID;
+			$ancestors = get_post_ancestors( $this_page_id );
+
+			if ( count($ancestors) === 1 ) {
+				// this is the main page of the guide so we can just list all of its children
+				$top_page = TRUE;
+				$guide_parent_id = $post->ID;
+			} else {
+				// it's not and we need to do get the full page tree
+				end($ancestors); // the topmost parent is actually the "guides" page so let's back it up one
+				$guide_parent_id = prev($ancestors); // much better
+				$children = wp_list_pages("title_li=&child_of=" . $guide_parent_id . "&echo=0");
+			}
+
+			// now get the complete tree of child pages for the guide's top page
+			$children = wp_list_pages('title_li=&child_of=' . $guide_parent_id . '&echo=0');
+			$attachments = get_posts( array(
+				'post_type' => 'attachment',
+				'posts_per_page' => -1,
+				'post_parent' => $guide_parent_id,
+				'exclude'     => get_post_thumbnail_id( $guide_parent_id ), //don't get the featured image
+			) );
+			?>
+
+			<nav class="guide-nav span3 navbar">
+				<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+				<div class="container clearfix">
+		      <a class="btn btn-navbar toggle-nav-bar" title="More">
+		        <div class="bars">
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+		        </div>
+		      </a>
+
+					<?php if ( $top_page ) { ?>
+						<h4><?php _e('In This Guide', 'cjet'); ?></h4>
+					<?php } else { ?>
+						<h4 class="guide-top"><a href="<?php echo get_permalink($guide_parent_id); ?>"><?php echo get_the_title($guide_parent_id); ?></a></h4>
+					<?php } ?>
+					<ul class="guide-tree">
+						<?php echo $children; ?>
+					</ul>
+
+					<?php
+					// on interior guide pages, list resources attached to the parent guide page
+					if ( !$top_page && $attachments ) : ?>
+					<div class="resources">
+						<h4><?php _e('Resources', 'cjet'); ?></h4>
+						<ul class="guide-resources"><?php
+							foreach ( $attachments as $attachment ) {
+								//print_r( $attachment );
+								$class = "mime-" . sanitize_title( $attachment->post_mime_type );
+								echo '<li class="' . $class . ' data-design-thumbnail">';
+								echo cjet_format_attachment_link( $attachment->ID );
+								echo '</li>';
+							}
+						?>
+						</ul></div>
+							<?php
+					endif;	// resources links
+					?>
+				</div>
+			</nav>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix span9'); ?>>
+
+				<header class="entry-header">
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<?php edit_post_link(__('Edit This Page', 'largo'), '<h5 class="byline"><span class="edit-link">', '</span></h5>'); ?>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+
+					<?php
+					// if we're on a guide "top" page, show author information and whatnot
+					// we can leverage Largo's author info widget here
+						if ( $top_page ) {
+							the_widget(
+								'largo_author_widget',
+								array( 'title' => __('Guide Author', 'cjet') )
+							);
+						}
+					?>
+
+					<?php // in-guide navigation ?>
+					<nav id="nav-below" class="pager post-nav clearfix">
+					<?php
+						$pagelist = get_pages('sort_column=menu_order&sort_order=asc&child_of=' . $guide_parent_id );
+						$pages = array();
+						$prev_id = $next_id = '';
+						foreach ($pagelist as $page) {
+						   $pages[] += $page->ID;
+						}
+
+						$current = array_search(get_the_ID(), $pages);
+						if ( array_key_exists($current-1, $pages)) {
+							$prev_id = $pages[$current-1];
+						}
+						if ( array_key_exists($current+1, $pages)) {
+							$next_id = $pages[$current+1];
+						}
+
+						if ( $top_page ) {
+							printf( '<div class="next"><a href="%1$s"><h5>Start Reading &rarr;</h5></a></div>',
+								get_permalink( $next_id ),
+								get_the_title( $next_id )
+							);
+						} else {
+							if (!empty($prev_id)) {
+								printf( '<div class="previous"><a href="%1$s"><h5>Previous Section</h5><span class="meta-nav">%2$s</span></a></div>',
+									get_permalink( $prev_id ),
+									get_the_title($prev_id)
+								);
+							}
+
+							if (!empty($next_id)) {
+								printf( '<div class="next"><a href="%1$s"><h5>Next Section</h5><span class="meta-nav">%2$s</span></a></div>',
+									get_permalink( $next_id ),
+									get_the_title( $next_id )
+								);
+							}
+						}
+						?>
+
+					</nav><!-- #nav-below -->
+				</div><!-- .entry-content -->
+
+			</article><!-- #post-<?php the_ID(); ?> -->
+
+		<?php endwhile; // end of the loop.
+	?>
+</div><!--#content-->
+
+<?php get_footer(); ?>
