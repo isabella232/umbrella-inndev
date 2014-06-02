9d20e3d5eeeaa3a45a6a73ff92b8262dff9409e8
adam schweigert <aschweigert@gmail.com>
2014-06-02 15:17:53 -0700

    adding other impaq.me theme files

---
+++ wp-content/themes/impaq/functions.php
@@ -0,0 +1,7 @@
+<?php
+function inn_typekit() { ?>
+	<script type="text/javascript" src="//use.typekit.net/mmy6iwx.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php
+}
+add_action( 'wp_head', 'inn_typekit' );
