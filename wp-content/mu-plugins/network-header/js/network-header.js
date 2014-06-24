0f47cca1d22e6b0fe7ccc6f61221c3536c828832
Ryan Nagle <rmnagle@gmail.com>
2014-06-24 13:00:28 -0500

    get the global header/nav working properly on mobile devices

--- wp-content/mu-plugins/network-header/js/network-header.js
+++ wp-content/mu-plugins/network-header/js/network-header.js
@@ -1,58 +1,67 @@
 (function() {
-	var $ = jQuery,
-		subNavHideTimeout,
-    intentTimeout;
+  var $ = jQuery,
+  subNavHideTimeout,
+  intentTimeout;
 
-	var displaySubNav = function(subNav) {
-		var navSelector = '.network-header nav',
-			subNavContainer = $(navSelector).find('.sub-nav-container');
+  var displaySubNav = function(subNav, name) {
+    var navSelector = '.network-header nav',
+    subNavContainer = $(navSelector).find('.sub-nav-container');
 
-		if (!subNavContainer.length) {
-			$(navSelector).find('.network-header-main-nav').after('<div class="sub-nav-container" />');
-			subNavContainer = $(navSelector).find('.sub-nav-container');
-		}
+    if (!subNavContainer.length) {
+      $(navSelector).find('.network-header-main-nav').after('<div class="sub-nav-container" />');
+      subNavContainer = $(navSelector).find('.sub-nav-container');
+    }
 
     if (subNav.length) {
+      subNavContainer.data('menu-name', name);
       subNavContainer.html(subNav.html());
       subNavContainer.slideDown(350);
     } else
       subNavContainer.slideUp(250);
-	}
+  }
 
-	var hideSubNav = function() {
-		var navSelector = '.network-header nav',
-		subNavContainer = $(navSelector).find('.sub-nav-container');
+  var hideSubNav = function() {
+    var navSelector = '.network-header nav',
+    subNavContainer = $(navSelector).find('.sub-nav-container');
 
-		if (!subNavContainer.length)
-			return false;
+    if (!subNavContainer.length)
+      return false;
 
-		subNavContainer.slideUp(250);
-	};
+    subNavContainer.slideUp(250);
+  };
 
-	$(function() {
-		$('ul.network-header-main-nav > li').on('mouseover', function(e) {
-			if (subNavHideTimeout)
-				clearTimeout(subNavHideTimeout);
+  var subNavIsVisible = function(name) {
+    return $('.sub-nav-container').is(':visible') && $('.sub-nav-container').data('menu-name') == name;
+  };
 
-      if (intentTimeout)
-        clearTimeout(intentTimeout);
-
-			var subNav = $(this).find('.network-header-sub-nav');
-      intentTimeout = setTimeout(displaySubNav.bind(null, subNav), 250);
-
-			return false;
-		});
-
-		$('.network-header').on('mouseleave', function(e) {
-			if (subNavHideTimeout)
-				clearTimeout(subNavHideTimeout);
+  $(function() {
+    var showMenu = function(e) {
+      if (subNavHideTimeout)
+        clearTimeout(subNavHideTimeout);
 
       if (intentTimeout)
         clearTimeout(intentTimeout);
 
-			subNavHideTimeout = setTimeout(hideSubNav, 500);
+      var menuName = $(this).find('> a').text();
+      if (!subNavIsVisible(menuName)) {
+        var subNav = $(this).find('.network-header-sub-nav');
+        intentTimeout = setTimeout(displaySubNav.bind(null, subNav, menuName), 250);
+        return false;
+      }
+    };
+    $('ul.network-header-main-nav > li').on('mouseover', showMenu);
+    $('ul.network-header-main-nav > li').on('touchstart', showMenu);
 
-			return false;
-		});
-	});
+    $('.network-header').on('mouseleave', function(e) {
+      if (subNavHideTimeout)
+        clearTimeout(subNavHideTimeout);
+
+      if (intentTimeout)
+        clearTimeout(intentTimeout);
+
+      subNavHideTimeout = setTimeout(hideSubNav, 500);
+
+      return false;
+    });
+  });
 })();
