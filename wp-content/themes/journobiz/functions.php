56a02b334a7e387d3508ead6475128e98b6a3aaa
cornershopcreative <hello@cornershopcreative.com>
2014-07-08 19:06:36 +0000

    Implementing custom meta fields for Grantees

--- wp-content/themes/journobiz/functions.php
+++ wp-content/themes/journobiz/functions.php
@@ -30,7 +30,7 @@ function jb_grantees() {
 		'description'         => __( 'Grant recipients', 'journobiz' ),
 		'labels'              => $labels,
 		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'revisions', ),
-		'taxonomies'          => array( 'category', 'post_tag' ),
+		'taxonomies'          => array( 'category' ),
 		'hierarchical'        => false,
 		'public'              => true,
 		'show_ui'             => true,
@@ -47,6 +47,35 @@ function jb_grantees() {
 	);
 	register_post_type( 'grantee', $args );
 
+	// And the taxonomy for Funding Rounds
+	$labels = array(
+		'name'                       => _x( 'Funding Rounds', 'Taxonomy General Name', 'journobiz' ),
+		'singular_name'              => _x( 'Round', 'Taxonomy Singular Name', 'journobiz' ),
+		'menu_name'                  => __( 'Funding Rounds', 'journobiz' ),
+		'all_items'                  => __( 'All Rounds', 'journobiz' ),
+		'parent_item'                => __( 'Parent Round', 'journobiz' ),
+		'parent_item_colon'          => __( 'Parent Round:', 'journobiz' ),
+		'new_item_name'              => __( 'New Round', 'journobiz' ),
+		'add_new_item'               => __( 'Add New Round', 'journobiz' ),
+		'edit_item'                  => __( 'Edit Round', 'journobiz' ),
+		'update_item'                => __( 'Update Round', 'journobiz' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'journobiz' ),
+		'search_items'               => __( 'Search Round', 'journobiz' ),
+		'add_or_remove_items'        => __( 'Add or remove rounds', 'journobiz' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'journobiz' ),
+		'not_found'                  => __( 'Not Found', 'journobiz' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => false,
+		'show_tagcloud'              => false,
+	);
+	register_taxonomy( 'round', array( 'grantee' ), $args );
+
 }
 
 // Hook into the 'init' action to register CPTs
@@ -58,6 +87,10 @@ add_action( 'init', 'jb_grantees', 0 );
  */
 require_once( get_template_directory() . "/inc/metabox-api.php");
 
+
+/**
+ * Add a metabox to posts to tag Grantees like categories
+ */
 largo_add_meta_box(
 	'post-grantees',
 	'Related Grantees',
@@ -89,9 +122,6 @@ function grantees_metabox_display() {
 	}
 	?>
 	<div id="posttype-grantees" class="posttypediv">
-		<?php
-		posts_by_grantee(140555);
-		?>
 		<div class="tabs-panel">
 			<ul id="posttype-grantees">
 			<?php
@@ -119,7 +149,7 @@ function grantees_metabox_display() {
  * largo metabox API doesn't yet support checkboxes as an input type
  * so we need a special function to save this field (for now)
  */
-function save_grantees($post_ID = 0) {
+function save_post_grantees($post_ID = 0) {
   $post_ID = (int) $post_ID;
   $post_type = get_post_type( $post_ID );
   $post_status = get_post_status( $post_ID );
@@ -131,7 +161,120 @@ function save_grantees($post_ID = 0) {
   }
   return $post_ID;
 }
-add_action('save_post', 'save_grantees');
+add_action('save_post', 'save_post_grantees');
+
+
+/**
+ * Metabox for extra fields for Grantees
+ */
+/**
+ * Loads the image management javascript
+ */
+function grantee_image_enqueue() {
+	global $typenow;
+	if( $typenow == 'grantee' ) {
+	    wp_enqueue_media();
+
+	    // Registers and enqueues the required javascript.
+	    wp_register_script( 'meta-box-file', get_stylesheet_directory_uri() . '/js/media-fields.js', array( 'jquery' ) );
+	    wp_localize_script( 'meta-box-file', 'meta_file',
+	        array(
+	            'title' => __( 'Select File', 'journobiz' ),
+	            'button' => __( 'Use this file', 'journobiz' ),
+	        )
+	    );
+	    wp_enqueue_script( 'meta-box-file' );
+	}
+}
+add_action( 'admin_enqueue_scripts', 'grantee_image_enqueue' );
+
+largo_add_meta_box(
+	'grantee-details',
+	'Grantee Details',
+	'grantee_details_display',
+	'grantee',
+	'normal',
+	'core'
+);
+
+function grantee_details_display() {
+	global $post;
+	$meta = get_post_meta( $post->ID, 'grantee_details', true );	// why third arg is true to get a serialized array is beyond me
+
+	//get filenames for friendlier display
+	if ( isset($meta['budget-id']) ) {
+		$meta['budget-filename'] = basename( get_attached_file( $meta['budget-id'] ));
+	}
+	if ( isset($meta['proposal-id']) ) {
+		$meta['proposal-filename'] = basename( get_attached_file( $meta['proposal-id'] ));
+	}
+
+	$simple_fields = array(
+		'org-name' => __( 'Organization Name', 'journobiz' ),
+		'award-amount' => __( 'Award Amount', 'journobiz' ),
+		'timeline-src' => __( 'TimelineJS URL', 'journobiz' )
+	);
+
+	?>
+	<div id="grantee-details">
+	<?php
+		foreach( $simple_fields as $field_id => $label ) {
+			?>
+			<p class="<?php esc_attr_e($field_id); ?>-wrapper">
+				<label for="<?php esc_attr_e($field_id); ?>"><?php echo $label; ?></label>
+				<input type="text" name="grantee_details[<?php esc_attr_e($field_id); ?>]" value="<?php esc_attr_e( $meta[$field_id] ); ?>" class="large-text" />
+			</p>
+			<?php
+		}
+	?>
+
+		<p class="why-wrapper">
+			<label for="why"><?php _e( 'Why Funded', 'journobiz' ) ?></label>
+			<textarea name="grantee_details[why]" class="large-text"><?php echo esc_textarea( $meta['why'] ); ?></textarea>
+		</p>
+
+		<p>
+    	<label for="meta-budget-filename" class="prfx-row-title"><?php _e( 'Budget PDF', 'journobiz' )?></label><br>
+			<input type="text" id="meta-budget-filename" class="regular-text" value="<?php if ( isset ( $meta['budget-filename'] ) ) echo $meta['budget-filename']; ?>" disabled="disabled" />
+			<input type="button" id="meta-budget-button" class="button" value="<?php _e( 'Select File', 'journobiz' )?>" />
+			<input type="hidden" name="grantee_details[budget-id]" id="meta-budget-id" value="<?php esc_attr_e( $meta['budget-id'] ); ?>">
+		</p>
+
+		<p>
+    	<label for="meta-proposal-filename" class="prfx-row-title"><?php _e( 'Proposal PDF', 'journobiz' )?></label><br>
+			<input type="text" id="meta-proposal-filename" class="regular-text" value="<?php if ( isset ( $meta['proposal-filename'] ) ) echo $meta['proposal-filename']; ?>" disabled="disabled" />
+			<input type="button" id="meta-proposal-button" class="button" value="<?php _e( 'Select File', 'journobiz' )?>" />
+			<input type="hidden" name="grantee_details[proposal-id]" id="meta-proposal-id" value="<?php esc_attr_e( $meta['proposal-id'] ); ?>">
+		</p>
+	</div>
+	<?php
+}
+
+/**
+ * Special function to save grantee fields
+ */
+function save_grantee_details($post_ID = 0) {
+  $post_ID = (int) $post_ID;
+  $post_type = get_post_type( $post_ID );
+  $post_status = get_post_status( $post_ID );
+	$fields = array('org-name', 'award-amount', 'timeline-src', 'why', 'budget-id', 'proposal-id' );
+
+  if ( $post_type && $post_type == 'grantee' ) {
+  	$grantee_details = array();
+  	foreach( $fields as $field ) {
+	  	if ( isset($_POST['grantee_details'][$field]) ) {
+		  	$grantee_details[$field] = $_POST['grantee_details'][$field];
+	  	}
+  	}
+  	update_post_meta( $post_ID, 'grantee_details', $grantee_details );
+  } else {
+  	delete_post_meta( $post_ID, 'grantee_details' );
+  }
+  return $post_ID;
+}
+add_action('save_post', 'save_grantee_details');
+
+
 
 /**
  * Convenience method for getting posts by grantee id
---
+++ wp-content/themes/journobiz/js/media-fields.js
@@ -0,0 +1,48 @@
+/*
+ * Attaches the image uploader to the input field
+ * See https://github.com/thomasgriffin/New-Media-Image-Uploader
+ */
+jQuery(document).ready(function($){
+
+  // Instantiates the variable that holds the media library frame.
+  var grantee_media_frame;
+
+  // Runs when the image button is clicked.
+  $('#meta-budget-button, #meta-proposal-button').click(function(e){
+
+		window.field_prefix = this.id.replace('-button', '');
+
+    // Prevents the default action from occuring.
+    e.preventDefault();
+
+    // If the frame already exists, re-open it.
+    if ( grantee_media_frame ) {
+      grantee_media_frame.open();
+      return;
+    }
+
+    // Sets up the media library frame
+    grantee_media_frame = wp.media.frames.grantee_media_frame = wp.media({
+  		className: "media-frame my-media-frame",
+      title: meta_file.title,
+      multiple: false,
+      button: { text:  meta_file.button },
+      library: { subtype: 'pdf', type: 'application' }
+    });
+
+    // Runs when an image is selected.
+    grantee_media_frame.on('select', function(){
+
+      // Grabs the attachment selection and creates a JSON representation of the model.
+      var media_attachment = grantee_media_frame.state().get('selection').first().toJSON();
+
+      // Sends the attachment URL to our custom image input field.
+      // See http://kingpro.me/article/tutorials/custom-image-uploader-for-plugins-using-the-wordpress-native-functions/ for object specs
+      $('#' + window.field_prefix + '-filename').val(media_attachment.filename);
+      $('#' + window.field_prefix + '-id').val(media_attachment.id);
+    });
+
+    // Opens the media library frame.
+    grantee_media_frame.open();
+  });
+});
