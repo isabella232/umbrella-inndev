6181d7c95fa6648723b1b3d5b270488420ae13e7
adam schweigert <aschweigert@gmail.com>
2014-08-12 17:01:57 -0400

    fixing up the headers

--- wp-content/themes/nerds/header.php
+++ wp-content/themes/nerds/header.php
@@ -44,8 +44,6 @@
 
 <div id="page" class="hfeed clearfix">
 
-	<?php // if (function_exists('dynamic_sidebar')) { dynamic_sidebar("Header"); } ?>
-
 	<header id="site-header" class="clearfix" itemscope itemtype="http://schema.org/Organization">
 		<?php largo_header(); ?>
 	</header>
