b4e97e07a1d7df50129f0c11918be46b8bc1309b
adam schweigert <aschweigert@gmail.com>
2014-08-19 14:55:04 -0400

    strip out nearly everything from the INN theme so we can start fresh

--- wp-content/themes/inn/functions.php
+++ wp-content/themes/inn/functions.php
@@ -1,9 +1,6 @@
 <?php
 
 //USEFUL CONSTANTS
-define( 'INN_FUNDER_PAGE_ID', 143094 );
-define( 'INN_ABOUT_PAGE_SLUG', 'about' );
-define( 'INN_GUIDE_PARENT_ID', 2500 );
 define( 'INN_MEMBER_TAXONOMY', 'ppu_focus_areas' );
 
 
@@ -11,7 +8,7 @@ define( 'INN_MEMBER_TAXONOMY', 'ppu_focus_areas' );
  * Load typekit stylesheet stuff
  */
 function inn_typekit() { ?>
-	<script type="text/javascript" src="//use.typekit.net/gzd1rgv.js"></script>
+	<script type="text/javascript" src="//use.typekit.net/mmy6iwx.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 <?php
 }
@@ -27,11 +24,6 @@ function inn_enqueue() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'inn_enqueue' );
-
-/**
- * Resources stuff
- */
-require_once( 'inn_resources.php' );
 
 
 /**
@@ -72,86 +64,6 @@ function inn_widgets() {
 
 }
 
-
-/**
- * Misc helper functions
- */
-
-//make sure links always start with HTTP, users often forget this
-function safe_url( $url ) {
-	$url = trim( $url );
-	if (strpos( $url, "http://" ) === 0 || strpos( $url, "https://" ) === 0) return $url;
-	return "http://" . $url;
-}
-
-// Decide whether or not we're on a page that shoudl show the sponsors
-function show_funders( $slug = NULL ) {
-
-	global $post;
-	$slug = ( $slug ) ? $slug : INN_ABOUT_PAGE_SLUG;
-
-	$parent_page_id = 0;
-	$about_page = 	get_page_by_path( $slug );
-	if ( $about_page ) $parent_page_id = $about_page->ID;
-
-	if ( is_front_page() || $post->ID == $parent_page_id || in_array( $parent_page_id, get_ancestors( $post->ID, 'page') ) ) {
-		return true;
-	}
-
-	return false;
-}
-
-
-/**
- * Shorthand for querying posts from a custom taxonomy
- * Used in homepage templates and sidebar widgets
- *
- * @param array $args query args
- * @return array of featured post objects
- * @since 1.0
- */
-function inn_get_featured_posts( $args = array() ) {
-    $defaults = array(
-    		'post_type' => array('post', 'network_content'),
-        'showposts' => 3,
-        'offset' 	=> 0,
-        'orderby' 	=> 'date',
-        'order' 	=> 'DESC',
-        'tax_query' => array(
-			array(
-				'taxonomy' 	=> 'prominence',
-				'field' 	=> 'slug',
-				'terms' 	=> 'footer-featured'
-			)
-		),
-        'ignore_sticky_posts' => 1,
-    );
-    $args = wp_parse_args( $args, $defaults );
-    $featured_query = new WP_Query( $args );
-    wp_reset_postdata();
-    return $featured_query;
-}
-
-/**
- * INN homepage credits: use a Member name if present, otherwise fall back to Largo technique
- */
-function inn_homepage_tag( $echo = false ) {
-	global $post, $tags;
-
-	//normal
-	if ( $post->post_type != 'network_content' && largo_has_categories_or_tags() ) {
-		return ( $echo ) ? largo_categories_and_tags(1) : true ;
-	}
-
-	//network content
-	$mem_id = $post->author;
-	$mem = get_userdata( $mem_id );
-	if ( !$mem ) {
-		return false;
-	} else {
-		return ( $echo ) ? printf('<a href="%s" title="Member page for %s">%s</a>', get_author_posts_url($mem_id), esc_attr($mem->display_name), $mem->display_name) : true ;
-	}
-}
 
 /**
  * Track things
