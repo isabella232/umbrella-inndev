b85dd9170aa87777d4c5114598489cfd4d7d86ee
adam schweigert <aschweigert@gmail.com>
2014-08-26 21:36:26 -0400

    add some styling for an email signup widget at the bottom of story pages

--- wp-content/themes/journobiz/header.php
+++ wp-content/themes/journobiz/header.php
@@ -122,17 +122,17 @@
 
 		<div class="newsletter-signup">
 				<form action="//investigativenewsnetwork.us1.list-manage.com/subscribe/post?u=81670c9d1b5fbeba1c29f2865&amp;id=e3e0b6be7f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-				<label>Subscribe to our free newsletter</label>
-				<fieldset>
-					<input required type="email" value="" name="EMAIL" class="required email_address" id="mce-EMAIL" placeholder="Email address">
-					<input required type="text" value="" name="FNAME" class="first_name" id="mce-FNAME" placeholder="First name">
-					<input required type="text" value="" name="LNAME" class="last_name" id="mce-LNAME" placeholder="Last name">
-					<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn submit">
-					<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-					<div style="position: absolute; left: -5000px;"><input type="text" name="b_81670c9d1b5fbeba1c29f2865_e3e0b6be7f" tabindex="-1" value=""></div>
-					<div class="error"></div>
-				</fieldset>
-			</form>
+					<label>Subscribe to our free newsletter</label>
+					<fieldset>
+						<input required type="email" value="" name="EMAIL" class="required email_address" id="mce-EMAIL" placeholder="Email address">
+						<input required type="text" value="" name="FNAME" class="first_name" id="mce-FNAME" placeholder="First name">
+						<input required type="text" value="" name="LNAME" class="last_name" id="mce-LNAME" placeholder="Last name">
+						<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn submit">
+						<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						<div style="position: absolute; left: -5000px;"><input type="text" name="b_81670c9d1b5fbeba1c29f2865_e3e0b6be7f" tabindex="-1" value=""></div>
+						<div class="error"></div>
+					</fieldset>
+				</form>
 		</div>
 	</header>
 	<?php endif; ?>
