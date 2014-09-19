c92fe9c78d6ccff0cb32f1781dfe9105f1b06564
adam schweigert <aschweigert@gmail.com>
2014-09-18 22:55:46 -0400

    latest largo

--- wp-content/themes/nerds/inc/users.php
+++ wp-content/themes/nerds/inc/users.php
@@ -12,6 +12,14 @@ function more_profile_info($user) { ?>
 				<span class="description">Please enter your job title.</span>
 			</td>
 		</tr>
+		<tr>
+			<th><label for="staff_widget">Staff Widget</label></th>
+			<td>
+				<input type="checkbox" name="hide" id="hide" value="no" <?php if (esc_attr( get_the_author_meta( "hdie", $user->ID )) == "yes") echo "checked"; ?> /><label for="hide"><?php _e("Hide in Widget"); ?></label><br />
+				<input type="checkbox" name="emeritus" id="emeritus" value="no" <?php if (esc_attr( get_the_author_meta( "emeritus", $user->ID )) == "yes") echo "checked"; ?> /><label for="emeritus"><?php _e("Emeritus?"); ?></label><br />
+				<input type="checkbox" name="honorary" id="honorary" value="no" <?php if (esc_attr( get_the_author_meta( "honorary", $user->ID )) == "yes") echo "checked"; ?> /><label for="honorary"><?php _e("Honorary?"); ?></label>
+			</td>
+		</tr>
 	</table>
 <?php }
 
@@ -20,4 +28,7 @@ add_action('edit_user_profile_update', 'save_more_profile_info');
 function save_more_profile_info($user_id) {
     if (!current_user_can('edit_user', $user_id )) { return false; }
     update_user_meta($user_id, 'job_title', $_POST['job_title']);
+    update_user_meta($user_id, 'hide', $_POST['hide']);
+    update_user_meta($user_id, 'emeritus', $_POST['emeritus']);
+    update_user_meta($user_id, 'honorary', $_POST['honorary']);
 }
