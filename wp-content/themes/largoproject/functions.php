54e914dc4d861a39a9c6f371580f13153e09a35c
adam schweigert <aschweigert@gmail.com>
2014-11-06 12:04:21 -0500

    add a few more odds and ends

---
+++ wp-content/themes/largoproject/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+// Includes
+$includes = array(
+	'/homepages/homepage.php'
+);
+foreach ( $includes as $include ) {
+	require_once( get_stylesheet_directory() . $include );
+}
+
+
+// Typekit
+function inn_typekit() { ?>
+	<script type="text/javascript" src="//use.typekit.net/mmy6iwx.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php
+}
+add_action( 'wp_head', 'inn_typekit' );
+
+
+// Add network header and footer
+add_action( 'largo_top', 'largo_render_network_header' );
+add_action( 'largo_before_footer_boilerplate', 'largo_render_network_footer' );
---
+++ wp-content/themes/largoproject/homepages/homepage.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once get_template_directory() . '/homepages/homepage-class.php';
+
+class LargoHomepageLayout extends Homepage {
+  var $name = 'Largo (Project Site) Homepage Layout';
+  var $description = 'Custom homepage layout for the Largo project site.';
+
+  function __construct($options=array()) {
+    $defaults = array(
+      'template' => get_stylesheet_directory() . '/homepages/templates/largo.php',
+      'assets' => array(
+	  		array('editorial', get_stylesheet_directory_uri() . '/homepages/assets/css/largo.css', array()),
+	  	)
+    );
+	$options = array_merge($defaults, $options);
+    $this->init();
+    $this->load($options);
+  }
+}
+
+function inn_custom_homepage_layouts() {
+	$unregister = array(
+		'HomepageBlog',
+		'HomepageSingle',
+		'HomepageSingleWithFeatured',
+		'HomepageSingleWithSeriesStories',
+		'TopStories',
+		'Slider'
+	);
+
+	foreach ($unregister as $layout)
+		unregister_homepage_layout($layout);
+
+	register_homepage_layout('LargoHomepageLayout');
+
+}
+add_action('init', 'inn_custom_homepage_layouts', 10);
