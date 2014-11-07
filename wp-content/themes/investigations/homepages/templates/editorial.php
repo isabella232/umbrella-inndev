92796afdff57886bdbf002c4efc27a4ad7ac9325
adam schweigert <aschweigert@gmail.com>
2014-11-07 15:16:03 -0500

    latest largo

--- wp-content/themes/investigations/homepages/templates/editorial.php
+++ wp-content/themes/investigations/homepages/templates/editorial.php
@@ -1,3 +1,19 @@
 <div id="content">
-	<h1>Something!</h1>
+	<?php
+    	switch_to_blog( 1 ); // network content is on the main blog because it depends on the member profiles
+
+    	$args = array(
+    		'post_type'			=> 'network_content',
+			'posts_per_page'	=> 10
+    	);
+		$query = new WP_Query( $args );
+
+		if ( $query->have_posts() ) {
+			while ( $query->have_posts() ) : $query->the_post();
+                get_template_part( 'partials/content', 'network-content' );
+			endwhile;
+        } else {
+            get_template_part( 'partials/content', 'not-found' );
+        }
+    ?>
 </div>
