2aa0984444071a144df03ebcb43dc92c36d20440
adam schweigert <aschweigert@gmail.com>
2015-01-26 16:08:14 -0500

    hero

--- wp-content/themes/largoproject/partials/largo-hero.php
+++ wp-content/themes/largoproject/partials/largo-hero.php
@@ -1,1 +1,6 @@
-<div class="site-hero"></div>
+<div class="site-hero">
+	<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/largo-sq.png" />
+	<h3 class="tagline">The WordPress Framework for News Websites</h3>
+	<a class="btn" href="https://github.com/INN/Largo/archive/master.zip">Download Largo</a>
+	<p class="version">Version 0.4</p>
+</div>
