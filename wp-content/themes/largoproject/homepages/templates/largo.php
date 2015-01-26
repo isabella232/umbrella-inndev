312be405c147b2e71b470647b79e9bea449d0555
adam schweigert <aschweigert@gmail.com>
2015-01-26 18:40:29 -0500

    styling the homepage

--- wp-content/themes/largoproject/homepages/templates/largo.php
+++ wp-content/themes/largoproject/homepages/templates/largo.php
@@ -1,20 +1,61 @@
 <div id="content" class="homepage-content clearfix span12">
 
 	<h2>Features</h2>
+	<h4>Designed for publishers, built for developers.</h4>
 	<div class="row-fluid">
-		<div class="span4"></div>
-		<div class="span4"></div>
-		<div class="span4"></div>
+		<div class="span4">
+			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/responsive.svg" />
+			<h5>Responsive Design</h5>
+			<p>Clean, modern and mobile first. Largo is designed to look great on any device.</p>
+		</div>
+		<div class="span4">
+			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/devfriendly.svg" />
+			<h5>Powerful Publishing Tools</h5>
+			<p>A real news theme for WordPress. Beyond design, Largo is built with the needs of news organizations in mind.</p>
+		</div>
+		<div class="span4">
+			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/devfriendly.svg" />
+			<h5>Developer Friendly</h5>
+			<p>Logical organization, documentation and a sample child theme make it easy to use Largo for your next project.</p>
+		</div>
+
+	</div>
+
+	<div class="row-fluid interstitial">
+		<h3>Largo is a project of INN</h3>
+		<p>INN is a nonprofit organization supporting a network of 100+ nonprofit news organizations. Projects like Largo depend on your support.</p>
+		<a class="btn btn-donate">Donate Now</a>
 	</div>
 
 	<h2>Support</h2>
+	<h4>Need assistance getting started with Largo? We're here to help.</h4>
 	<div class="row-fluid">
-		<div class="span4"></div>
-		<div class="span4"></div>
-		<div class="span4"></div>
+		<div class="span4">
+			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/docs.svg" />
+			<h5>Documentation</h5>
+			<p>Comprehensive documentation helps you get up and running with Largo and explains many of our more advanced features.</p>
+		</div>
+		<div class="span4">
+			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/faq.svg" />
+			<h5>Knowledgebase</h5>
+			<p>Answers to our most frequently asked questions.</p>
+		</div>
+		<div class="span4">
+			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/helpdesk.svg" />
+			<h5>Help Desk</h5>
+			<p>Need more help? Submit a support request.</p>
+		</div>
+	</div>
+
+	<div class="row-fluid interstitial">
+		<h3>Latest News</h3>
+		<?php
+			//latest post tagged "largo project" from the nerd blog
+		?>
 	</div>
 
 	<h2>Showcase</h2>
+	<h4>Over 100 sites are using Largo. Here are a few:</h4>
 	<div class="row-fluid">
 		<div class="span4"></div>
 		<div class="span4"></div>
