8ab9963bed5c7d4e3f9fa48485232f129bb60906
adam schweigert <aschweigert@gmail.com>
2015-01-26 19:20:11 -0500

    more homepage styles

--- wp-content/themes/largoproject/homepages/templates/largo.php
+++ wp-content/themes/largoproject/homepages/templates/largo.php
@@ -1,51 +1,55 @@
 <div id="content" class="homepage-content clearfix span12">
 
-	<h2>Features</h2>
-	<h4>Designed for publishers, built for developers.</h4>
-	<div class="row-fluid">
-		<div class="span4">
-			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/responsive.svg" />
-			<h5>Responsive Design</h5>
-			<p>Clean, modern and mobile first. Largo is designed to look great on any device.</p>
-		</div>
-		<div class="span4">
-			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/devfriendly.svg" />
-			<h5>Powerful Publishing Tools</h5>
-			<p>A real news theme for WordPress. Beyond design, Largo is built with the needs of news organizations in mind.</p>
-		</div>
-		<div class="span4">
-			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/devfriendly.svg" />
-			<h5>Developer Friendly</h5>
-			<p>Logical organization, documentation and a sample child theme make it easy to use Largo for your next project.</p>
+	<section>
+		<h2>Features</h2>
+		<h4>Designed for publishers, built for developers.</h4>
+		<div class="row-fluid">
+			<div class="span4">
+				<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/responsive.svg" />
+				<h5>Responsive Design</h5>
+				<p>Clean, modern and mobile first. Largo is designed to look great on any device.</p>
+			</div>
+			<div class="span4">
+				<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/devfriendly.svg" />
+				<h5>Powerful Publishing Tools</h5>
+				<p>A real news theme for WordPress. Beyond design, Largo is built with the needs of news organizations in mind.</p>
+			</div>
+			<div class="span4">
+				<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/devfriendly.svg" />
+				<h5>Developer Friendly</h5>
+				<p>Logical organization, <a href="http://largo.readthedocs.org/developers/fordevelopers.html">documentation</a> and <a href="https://github.com/INN/Largo-Sample-Child-Theme">a sample child theme</a> make it easy to use Largo for your next project.</p>
+			</div>
 		</div>
 
-	</div>
+	</section>
 
 	<div class="row-fluid interstitial">
 		<h3>Largo is a project of INN</h3>
-		<p>INN is a nonprofit organization supporting a network of 100+ nonprofit news organizations. Projects like Largo depend on your support.</p>
-		<a class="btn btn-donate">Donate Now</a>
+		<p><a href="http://investigativenewsnetwork.org/">INN</a> is a nonprofit organization supporting a network of <a href="http://investigativenewsnetwork.org/member/">100+ nonprofit news organizations</a>. Projects like Largo depend on your support.</p>
+		<a class="btn btn-donate" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=M7T6234BREMG2">Donate Now</a>
 	</div>
 
-	<h2>Support</h2>
-	<h4>Need assistance getting started with Largo? We're here to help.</h4>
-	<div class="row-fluid">
-		<div class="span4">
-			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/docs.svg" />
-			<h5>Documentation</h5>
-			<p>Comprehensive documentation helps you get up and running with Largo and explains many of our more advanced features.</p>
+	<section>
+		<h2>Support</h2>
+		<h4>Need assistance getting started with Largo? We're here to help.</h4>
+		<div class="row-fluid">
+			<div class="span4">
+				<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/docs.svg" />
+				<h5>Documentation</h5>
+				<p>Comprehensive documentation helps you get up and running with Largo and explains many of our more advanced features.</p>
+			</div>
+			<div class="span4">
+				<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/faq.svg" />
+				<h5>Knowledgebase</h5>
+				<p>Answers to our most frequently asked questions.</p>
+			</div>
+			<div class="span4">
+				<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/helpdesk.svg" />
+				<h5>Help Desk</h5>
+				<p>Need more help? Submit a support request.</p>
+			</div>
 		</div>
-		<div class="span4">
-			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/faq.svg" />
-			<h5>Knowledgebase</h5>
-			<p>Answers to our most frequently asked questions.</p>
-		</div>
-		<div class="span4">
-			<img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/helpdesk.svg" />
-			<h5>Help Desk</h5>
-			<p>Need more help? Submit a support request.</p>
-		</div>
-	</div>
+	</section>
 
 	<div class="row-fluid interstitial">
 		<h3>Latest News</h3>
@@ -54,12 +58,14 @@
 		?>
 	</div>
 
-	<h2>Showcase</h2>
-	<h4>Over 100 sites are using Largo. Here are a few:</h4>
-	<div class="row-fluid">
-		<div class="span4"></div>
-		<div class="span4"></div>
-		<div class="span4"></div>
-	</div>
+	<section>
+		<h2>Showcase</h2>
+		<h4>Over 100 sites are using Largo. Here are a few:</h4>
+		<div class="row-fluid">
+			<div class="span4"></div>
+			<div class="span4"></div>
+			<div class="span4"></div>
+		</div>
+	</section>
 
 </div>
