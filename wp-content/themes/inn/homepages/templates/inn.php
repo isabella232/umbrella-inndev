e68b8dec19e71591ef9f769d8bcb135c96570a4b
adam schweigert <aschweigert@gmail.com>
2015-01-29 17:22:56 -0500

    more homepage stuff

--- wp-content/themes/inn/homepages/templates/inn.php
+++ wp-content/themes/inn/homepages/templates/inn.php
@@ -1,3 +1,5 @@
+<?php $img_path = get_stylesheet_directory_uri() . '/homepages/assets/img/'; ?>
+
 <section id="hero">
 	<div class="content">
 		<h1>Institute for Nonprofit News</h1>
@@ -5,24 +7,136 @@
 	</div>
 </section>
 
-<section id="about">
-</section>
-
-<section id="email" class="interstitial">
+<section id="about" class="normal">
 	<div class="content">
-
+		<h3>About INN</h3>
+		<div class="row-fluid">
+			<div class="span4">
+				<img class="icon" src="<? echo $img_path . 'icons/missionglobe.svg'; ?>" />
+				<h5>Mission</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span4">
+				<img class="icon" src="<? echo $img_path . 'icons/people.svg'; ?>" />
+				<h5>People</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span4">
+				<img class="icon" src="<? echo $img_path . 'icons/news.svg'; ?>" />
+				<h5>News</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span4">
+				<img class="icon" src="<? echo $img_path . 'icons/financial.svg'; ?>" />
+				<h5>Financial</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span4">
+				<img class="icon" src="<? echo $img_path . 'icons/legal.svg'; ?>" />
+				<h5>Legal</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span4">
+				<img class="icon" src="<? echo $img_path . 'icons/contact.svg'; ?>" />
+				<h5>Contact</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+		</div>
 	</div>
 </section>
 
-<section id="programs">
+<section id="email" class="normal">
+	<div class="content">
+		<img class="mail-icon" src="<? echo $img_path . 'icons/mail.svg'; ?>" />
+		<h3>Getcha some email</h3>
+		<p>in your inbox, duh. <strong>Catch it!</strong></p>
+		<form>
+			asdfasdf
+		</form>
+	</div>
+</section>
+
+<section id="programs" class="normal">
+	<div class="content">
+		<h3>Programs</h3>
+		<div class="row-fluid">
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/missionglobe.svg'; ?>" />
+				<h5>Mission</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/people.svg'; ?>" />
+				<h5>People</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/news.svg'; ?>" />
+				<h5>News</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/news.svg'; ?>" />
+				<h5>News</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/missionglobe.svg'; ?>" />
+				<h5>Mission</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/people.svg'; ?>" />
+				<h5>People</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/news.svg'; ?>" />
+				<h5>News</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/news.svg'; ?>" />
+				<h5>News</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+		</div>
+	</div>
 </section>
 
 <section id="members" class="interstitial">
-	<div class="content">
-	</div>
 </section>
 
-<section id="member-info">
+<section id="member-info" class="normal">
+	<div class="content">
+		<h3>Membership Info</h3>
+		<div class="row-fluid">
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/missionglobe.svg'; ?>" />
+				<h5>Member Benefits</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/people.svg'; ?>" />
+				<h5>Membership Standards</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/news.svg'; ?>" />
+				<h5>How To Join</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+			<div class="span3">
+				<img class="icon" src="<? echo $img_path . 'icons/news.svg'; ?>" />
+				<h5>FAQ</h5>
+				<p>What we do and all of that stuff</p>
+			</div>
+		</div>
+	</div>
 </section>
 
 <section class="interstitial">
@@ -30,5 +144,5 @@
 	</div>
 </section>
 
-<section id="supporters">
+<section id="supporters" class="normal">
 </section>
