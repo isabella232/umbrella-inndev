946b9f71cfda3e646499bd2e068874d2940f34a8
Ryan Nagle <rmnagle@gmail.com>
2015-03-03 09:06:51 -0500

    remove the js that controls member info modals in the member widget

--- wp-content/themes/inn/js/inn.js
+++ wp-content/themes/inn/js/inn.js
@@ -1,29 +1,5 @@
 jQuery(document).ready( function($) {
 
-	//membership widget behaviors
-	$(".inn-members-widget .members a").on( 'click', function( ) {
-		var member_url = this.href,
-			the_link = this;
-		$(this).addClass('icon-spinner');
-		$('.member-details').load( member_url + " article.inn_member", function ( resp, status, xhr ) {
-			$(this).parent().fadeIn('fast');
-			$("body").addClass('member-widget-open');
-			$(the_link).removeClass('icon-spinner');
-		});
-		return false;
-	});
-
-	$(".member-details-wrapper .close").on( 'click', function() {
-		$(this).parent().fadeOut('fast');
-		$("body").removeClass('member-widget-open');
-	});
-
-	$(".members-menu a").each( function() {
-		if ( this.href ==  window.location.href.slice(0, -1) || this.href == window.location.href ) {
-			$(this).parent().remove();
-		}
-	});
-
 	// Adding classes to header social for CSS hiding
 	$('#header-social i').each( function() {
 		$(this).closest('li').addClass( $(this).attr('class') + '-parent' );
@@ -42,6 +18,5 @@ jQuery(document).ready( function($) {
 	$('.member-nav select').on('change', function() {
 		window.location = $(this).val();
 	});
-
 
 });
