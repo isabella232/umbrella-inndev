11e5225b4298b275910f7bbd0231ea15228a12c0
Ryan Nagle <rmnagle@gmail.com>
2015-03-03 09:14:12 -0500

    add the header background to the 'our members' section of the homepage

--- wp-content/themes/inn/homepages/templates/inn.php
+++ wp-content/themes/inn/homepages/templates/inn.php
@@ -136,7 +136,7 @@
 	</div>
 </section>
 
-<section id="members">
+<section id="members" class="normal">
 	<h3><a href="/members/"><span>Our Members</span></a></h3>
 	<?php the_widget ( 'members_widget' ); ?>
 </section>
