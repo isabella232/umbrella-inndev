a3ebe1d139590c51a572cea98dbdb7f5a50bb1dd
Ryan Nagle <rmnagle@gmail.com>
2015-03-09 16:30:30 -0500

    update the Vagrantfile, update to largo, modify some partials for compatibility with recent changes to largo

--- wp-content/themes/inn/partials/content-news.php
+++ wp-content/themes/inn/partials/content-news.php
@@ -1,19 +1,16 @@
 <h1 class="entry-title">Network News</h1>
 <?php
 	global $wp_query;
-	$query_args = array (
-		'posts_per_page'	=> 10,
-		'post_status'		=> 'publish',
+	$query_args = array(
+		'posts_per_page' => 10,
+		'post_status' => 'publish',
 	);
 	$wp_query = new WP_Query( $query_args );
 
-	wp_localize_script(
-		'load-more-posts', 'LMP', array(
-			'ajax_url' => admin_url('admin-ajax.php'),
-			'paged' => (!empty($wp_query->query_vars['paged']))? $wp_query->query_vars['paged'] : 0,
-			'query' => $wp_query->query
-		)
-	);
+	add_filter('largo_load_more_posts_json', function($LMP) use ($query_args) {
+		$LMP['query'] = $query_args;
+		return $LMP;
+	});
 
 	if ( $wp_query->have_posts() ) {
 		while ( $wp_query->have_posts() ) : $wp_query->the_post();
