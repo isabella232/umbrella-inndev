a9e52942e7174266bd790ea8d160e403a0d329e8
aschweigert <aschweigert@gmail.com>
2016-01-22 10:59:40 -0500

    add innovation fund banner

--- wp-content/themes/inn/partials/alert.php
+++ wp-content/themes/inn/partials/alert.php
@@ -1,3 +1,3 @@
 <div class="alert">
-	<p>Applications for Round 4 of INN's <strong><a href="http://journo.biz/innovation-fund/">INNovation fund</a></strong> are now open! <strong><a href="http://journo.biz/innovation-fund/">Learn&nbsp;More&nbsp;&raquo;</a></strong></p>
+	<p>INN announces 15 winners in fourth round of <a href="http://journo.biz/innovation-fund/">INNovation Fund</a> <strong><a href="/2016/01/inn-announces-15-winners-in-fourth-round-of-innovation-fund/">Learn&nbsp;More&nbsp;&raquo;</a></strong></p>
 </div>
