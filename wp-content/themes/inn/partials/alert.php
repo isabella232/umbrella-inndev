bf10eece46d67862ec0c7c98a6820031796392c1
aschweigert <aschweigert@gmail.com>
2016-05-16 12:38:24 -0400

    update alert banner

--- wp-content/themes/inn/partials/alert.php
+++ wp-content/themes/inn/partials/alert.php
@@ -1,3 +1,3 @@
 <div class="alert">
-	<p>INN announces 15 winners in fourth round of <a href="http://journo.biz/innovation-fund/">INNovation Fund</a> <strong><a href="/2016/01/inn-announces-15-winners-in-fourth-round-of-innovation-fund/">Learn&nbsp;More&nbsp;&raquo;</a></strong></p>
+	<p><strong><a href="http://inn.org/inn-day-2016/">Register now</a></strong> for Marketing + Branding and Donor Cultivation workshops at INN Day (June 15-16 in New Orleans).</p>
 </div>
