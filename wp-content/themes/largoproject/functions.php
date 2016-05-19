8f3d201f960393b6ef42e45b73599f9ac1293dfb
aschweigert <aschweigert@gmail.com>
2016-05-19 17:24:46 -0400

    use different hook for largo hero

--- wp-content/themes/largoproject/functions.php
+++ wp-content/themes/largoproject/functions.php
@@ -20,4 +20,4 @@ add_action( 'wp_head', 'inn_typekit' );
 function largo_load_hero() {
 	get_template_part( 'partials/largo', 'hero' );
 }
-add_action( 'largo_top', 'largo_load_hero'  );
+add_action( 'largo_after_global_nav', 'largo_load_hero'  );
