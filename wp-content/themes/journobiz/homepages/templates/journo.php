5174c79a18e59795aead7e17e733d787154edac7
aschweigert <aschweigert@gmail.com>
2016-05-19 12:29:50 -0400

    innovation.inn.org should just use the innovation fund page as the homepage

--- wp-content/themes/journobiz/homepages/templates/journo.php
+++ wp-content/themes/journobiz/homepages/templates/journo.php
@@ -1,77 +1,119 @@
-<?php
+<?php $my_query = new WP_Query( array( 'page_id' => 140698 ) ); ?>
 
-global $shown_ids;
-
-$bigStoryPost = largo_home_single_top();
-$featuredStories = largo_home_featured_stories();
-$shown_ids[] = $bigStoryPost->ID; //don't repeat the current post
-
-?>
-<div id="homepage-featured" class="row-fluid clearfix">
-	<div class="home-single span9">
-		<div class="home-top">
-			<a href="<?php echo esc_attr(get_permalink($bigStoryPost->ID)); ?>">
-				<?php echo get_the_post_thumbnail($bigStoryPost->ID, 'full'); ?>
-			</a>
-			<div id="dark-top">
-				<?php echo $bigStory; // Big story zone ?>
-			</div>
-		</div>
-	</div>
-	<div class="span3">
-		<?php $substories = largo_get_featured_posts( array(
-			'tax_query' => array(
-				array(
-					'taxonomy' 	=> 'prominence',
-					'field' 	=> 'slug',
-					'terms' 	=> 'homepage-featured'
-				)
-			),
-			'showposts'		=> 3,
-			'post__not_in' 	=> $shown_ids
-		) );
-		if ( $substories->have_posts() ) :
-			while ( $substories->have_posts() ) : $substories->the_post(); $shown_ids[] = get_the_ID();
-		?>
-				<div class="story">
-					<a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail(get_the_id(), 'large'); ?></a>
-			        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-			    </div>
-		<?php
-			endwhile;
-		endif; // end more featured posts ?>
-	</div>
-</div>
-<div id="homepage-bottom">
-	<div class="span9">
+<div id="grantee-content" class="span12" role="main">
 	<?php
-			$args = array(
-				'paged'					=> $paged,
-				'post_status'			=> 'publish',
-				'posts_per_page'		=> 10,
-				'post__not_in' 			=> $shown_ids,
-				);
-
-			$query = new WP_Query( $args );
-
-			if ( $query->have_posts() ) {
-				while ( $query->have_posts() ) : $query->the_post();
-					//if the post is in the array of post IDs already on this page, skip it. Just a double-check
-					if ( in_array( get_the_ID(), $shown_ids ) ) {
-						continue;
-					} else {
-						$ids[] = get_the_ID();
-						get_template_part( 'partials/content', 'home' );
-					}
-				endwhile;
-				//largo_content_nav( 'nav-below' );
-			} else {
-				get_template_part( 'partials/content', 'not-found' );
-			}
+		while ( $my_query->have_posts() ) : $my_query->the_post();
+		global $post;
 	?>
-	</div>
-	<div class="span3">
-		<h2>INNovation Fund</h2>
-		<?php dynamic_sidebar( 'sidebar-home' ); ?>
-	</div>
-</div>
+	<article id="post-<?php the_ID(); ?>" <?php post_class( 'hnews item' ); ?> itemscope itemtype="http://schema.org/Article">
+		<header>
+			<div class="img-container"><?php the_post_thumbnail('full'); ?></div>
+			<div class="info span5">
+	 			<h1 class="entry-title" itemprop="headline"><?php the_title(); ?></h1>
+		 			<?php the_content(); ?>
+			</div>
+	 		<meta itemprop="description" content="<?php echo strip_tags(largo_excerpt( $post, 5, false, '', false ) ); ?>" />
+	 		<meta itemprop="datePublished" content="<?php echo get_the_date( 'c' ); ?>" />
+	 		<meta itemprop="dateModified" content="<?php echo get_the_modified_date( 'c' ); ?>" />
+	 		<?php
+	 			if ( has_post_thumbnail( $post->ID ) ) {
+					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' );
+					echo '<meta itemprop="image" content="' . $image[0] . '" />';
+				}
+	 		?>
+		</header><!-- / entry header -->
+
+		<?php
+			// fetch child pages of the current page. Loop thru and show them, querying Round terms for grantees to include based on the given Page's assigned rounds
+			$children = new WP_Query( array(
+				'post_type' => 'page',
+				'post_parent' => get_the_ID(),
+				'post_status' => array('publish','future'),
+				'orderby' => 'menu_order'
+			) );
+
+			if ( $children->have_posts() ): ?>
+				<ul class="funding-rounds">
+				<?php
+
+				while ( $children->have_posts() ) :
+					$children->the_post();
+					global $more;
+					$more = 0;
+					$round_live = ( get_post_status() == 'publish' ) ? true : false ;
+					if ( $round_live ) : ?>
+					<li class="round active">
+						<article>
+							<header>
+								<h1><?php the_title(); ?></h1>
+							</header>
+							<div class="intro">
+								<?php the_content( __('Read More', 'journobiz') ); ?>
+							</div>
+								<?php
+									// get grantees belonging to the round(s) this child page is assigned to
+
+									$rounds = wp_get_post_terms( get_the_ID(), 'round', array('fields' => 'ids') );
+									$grantees = new WP_Query( array(
+										'post_type' => 'grantee',
+										'post_status' => 'publish',
+										'orderby' => 'title',
+										'tax_query' => array(
+											array(
+												'taxonomy' => 'round',
+												'field' => 'id',
+												'terms' => $rounds
+											)
+										)
+									) );
+									$count = 1;
+									if ( $grantees->have_posts() ) :
+										$round_post = $post; ?>
+										<div class="grantee-list">
+
+									<?php
+									while ( $grantees->have_posts() ) : $grantees->the_post();
+										$details = get_post_meta( get_the_ID(), 'grantee_details', true );
+										//if ( $count === 1 || $count % 3 === 1 ) echo '<div class="row-fluid">';
+									?>
+										<div class="grantee <?php echo 'item-' . $count; ?>">
+											<?php the_post_thumbnail( 'thumbnail' ); ?>
+											<div>
+												<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+												<p class="org-name"><?php echo $details['org-name']; ?></p>
+												<p class="award-amt"><?php echo __('Award Amount: ', 'journobiz') . $details['award-amount']; ?></p>
+											</div>
+										</div>
+									<?php
+									//if ( $count % 3 === 0) echo '</div>';
+									$count++;
+									endwhile;
+									setup_postdata( $round_post );
+									$post = $round_post; ?>
+							</div>
+							<?php
+								endif;
+							?>
+						</article>
+					</li>
+					<?php else : ?>
+					<li class="round future">
+						<header>
+							<h1><?php echo str_replace("Round ", "", get_the_title() ); ?></h1>
+						</header>
+						<div class="intro">
+							<?php //largo_excerpt( get_the_ID(), 1, false ); ?>
+							<?php the_content( __('Read More', 'journobiz') ); ?>
+						</div>
+					</li>
+			<?php
+					endif;
+				endwhile;
+				wp_reset_postdata(); ?>
+				</ul>
+		<?php
+			endif;
+		endwhile;
+		?>
+
+</div><!--#content-->
