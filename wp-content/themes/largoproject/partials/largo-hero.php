1c3e0879bcca2e9844bf714706ab93e900ace57d
aschweigert <aschweigert@gmail.com>
2016-07-07 11:50:40 -0400

    update gravity forms styles

--- wp-content/themes/largoproject/partials/largo-hero.php
+++ wp-content/themes/largoproject/partials/largo-hero.php
@@ -1,6 +1,10 @@
+<?php
+	if ( is_home() ) {
+?>
 <div class="site-hero">
 	<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/largo-sq.png" />
 	<h3 class="tagline">The WordPress Framework for News Websites</h3>
 	<a class="btn" href="https://github.com/INN/Largo/archive/master.zip">Download Largo</a>
 	<p class="version">Version 0.5.4</p>
 </div>
+<?php }
