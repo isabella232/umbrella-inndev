53b5dfea21d3703bf91059d83b72070d78fdfebb
Ben Keith <ben@benlk.com>
2017-01-11 18:04:12 -0500

    before deleting the 'news' theme

--- wp-content/themes/news/homepages/homepage.php
+++ wp-content/themes/news/homepages/homepage.php
@@ -3,20 +3,20 @@
 include_once get_template_directory() . '/homepages/homepage-class.php';
 
 class EditorialHomepageLayout extends Homepage {
-  var $name = 'INN Editorial Site Homepage Layout';
-  var $description = 'Custom homepage layout for the INN editorial projects site.';
+	var $name = 'INN Editorial Site Homepage Layout';
+	var $description = 'Custom homepage layout for the INN editorial projects site.';
 
-  function __construct($options=array()) {
-    $defaults = array(
-      'template' => get_stylesheet_directory() . '/homepages/templates/editorial.php',
-      'assets' => array(
-	  		array('editorial', get_stylesheet_directory_uri() . '/homepages/assets/css/editorial.css', array()),
-	  	)
-    );
+	function __construct($options=array()) {
+		$defaults = array(
+			'template' => get_stylesheet_directory() . '/homepages/templates/editorial.php',
+			'assets' => array(
+				array('editorial', get_stylesheet_directory_uri() . '/homepages/assets/css/editorial.css', array()),
+			)
+		);
 	$options = array_merge($defaults, $options);
-    $this->init();
-    $this->load($options);
-  }
+		$this->init();
+		$this->load($options);
+	}
 }
 
 function inn_custom_homepage_layouts() {
