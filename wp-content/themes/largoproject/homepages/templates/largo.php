73263dc265c3af9e223f4dc0c199dee693863c10
Gabriel Hongsdusit <gabriel@inn.org>
2017-01-18 10:57:57 -0800

    add why largo section

--- wp-content/themes/largoproject/homepages/templates/largo.php
+++ wp-content/themes/largoproject/homepages/templates/largo.php
@@ -1,5 +1,136 @@
 <div id="content" class="homepage-content clearfix span12">
 
+	<section class="largo-section">
+		<h2>Why Largo?</h2>
+		<h4>Picto tem denet omnit expliame non reror ati reictio volores.</h4>
+		<div class="row-fluid clearfix">
+			<div class="about-description-1">
+				<div class="illustration">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/illustrations/responsive-design.png" />
+				</div>
+				<div class="description">
+					<h3>Responsive Design</h3>
+					<p>Clean, modern and mobile first. Largo is designed to look great on any device.</p>
+				</div>
+			</div>
+			<div class="about-description-1 about-description-2">
+				<div class="illustration">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/illustrations/powerful-tools.png" />
+				</div>
+				<div class="description">
+					<h3>Powerful Publishing Tools</h3>
+					<p>Unlike other WordPress "news" themes, Largo is built by real news nerds. We obsess about workfow, too.</p>
+				</div>
+			</div>
+			<div class="about-description-1">
+				<div class="illustration">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/illustrations/developer-friendly.png" />
+				</div>
+				<div class="description">
+					<h3>Developer Friendly</h3>
+					<p>Logical organization, documentation and a sample child theme make it easy to use Largo for your next project.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<div class="row-fluid interstitial">
+		<h3>Largo is a project of INN</h3>
+		<p><a href="http://inn.org/">INN</a> is a nonprofit organization supporting a network of <a href="http://inn.org/members/">120 nonprofit news organizations</a>. Projects like Largo depend on your support.</p>
+		<a class="btn btn-donate btn-primary" href="https://inn.org/donate/">Donate Now</a>
+	</div>
+
+	<section>
+		<h2>Support</h2>
+		<h4>Need assistance getting started with Largo? We're here to help.</h4>
+		<div class="row-fluid">
+			<div class="span4">
+				<a href="http://largo.readthedocs.org/"><img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/docs.svg" /></a>
+				<h5><a href="http://largo.readthedocs.org/">Documentation</a></h5>
+				<p>Get up and running with Largo and explore many of our more advanced features.</p>
+			</div>
+			<div class="span4">
+				<a href="http://support.largoproject.org/support/solutions"><img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/faq.svg" /></a>
+				<h5><a href="http://support.largoproject.org/support/solutions">Knowledge Base</a></h5>
+				<p>Answers to our most frequently asked questions.</p>
+			</div>
+			<div class="span4">
+				<a href="http://support.largoproject.org"><img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/helpdesk.svg" /></a>
+				<h5><a href="http://support.largoproject.org">Help Desk</a></h5>
+				<p>Need more help? <a href="http://support.largoproject.org">Submit a support request</a>.</p>
+			</div>
+		</div>
+	</section>
+
+	<div class="row-fluid interstitial news">
+		<h3>Latest News</h3>
+		<?php
+			switch_to_blog( 7 );
+
+			$args = array (
+				'showposts' => 1,
+				'post_status' => 'publish',
+				'cat' => 130 // Largo
+			);
+
+			$query = new WP_Query( $args );
+
+          	if ( $query->have_posts() ) {
+          		while ( $query->have_posts() ) : $query->the_post();
+          			echo '<h5><a href="' . get_permalink() . '">' . get_the_title() . '</a></h5>';
+          			largo_excerpt();
+          			echo '<p class="more"><a href="http://nerds.inn.org/category/largo/">More Project Updates</a></p>';
+          		endwhile;
+          	}
+
+			restore_current_blog();
+		?>
+	</div>
+
+	<section id="showcase">
+		<h2>Showcase</h2>
+		<h4>Hundreds of sites are using Largo. Here are a few:</h4>
+		<div class="row-fluid">
+			<div class="span4">
+				<a href="http://wisconsinwatch.org"><img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/screenshots/wcij.png" /></a>
+			</div>
+			<div class="span4">
+				<a href="http://current.org"><img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/screenshots/current.png" /></a>
+			</div>
+			<div class="span4">
+				<a href="http://kycir.org"><img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/screenshots/kycir.png" /></a>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span4">
+				<a href="http://cornellsun.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/screenshots/cornellsun.png" /></a>
+			</div>
+			<div class="span4">
+				<a href="http://periodismoinvestigativo.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/screenshots/cpipr.png" /></a>
+			</div>
+			<div class="span4">
+				<a href="http://aspenjournalism.org/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/screenshots/aspen.png" /></a>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span4">
+				<a href="http://midwestenergynews.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/screenshots/mwen.png" /></a>
+			</div>
+			<div class="span4">
+				<a href="http://womensenews.org/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/screenshots/womensenews.png" /></a>
+			</div>
+			<div class="span4">
+				<a href="http://gijn.org"><img src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/screenshots/gijn.png" /></a>
+			</div>
+		</div>
+	</section>
+
+</div>
+
+<!--
+
+<div id="content" class="homepage-content clearfix span12">
+
 	<section>
 		<h2>Features</h2>
 		<h4>Designed for publishers, built for developers.</h4>
@@ -117,3 +248,5 @@
 	</section>
 
 </div>
+
+-->
