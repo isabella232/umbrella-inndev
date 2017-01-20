f14fea3ae0aba1a60697cde0e52ff0f888b656e5
Gabriel Hongsdusit <gabriel@inn.org>
2017-01-20 13:35:24 -0800

    add ProProject Updates section

--- wp-content/themes/largoproject/homepages/templates/largo.php
+++ wp-content/themes/largoproject/homepages/templates/largo.php
@@ -146,12 +146,21 @@
 		</div>
 	</section>
 	<section id="project-updates" class="largo-section">
-		<h2>Project Updates</h2>
-		<div class="max-width-container">
+		<h2>Latest Updates</h2>
+		<div class="max-width-container clearfix">
 			<div class="update-row">
-				<span>12.05.16</span>
+				<span class="update-date">12.05.16</span>
 				<a href="#">Quo tem elit ut alit porecto il id</a>
 			</div>
+			<div class="update-row">
+				<span class="update-date">11.15.16</span>
+				<a href="#">Us eum sin nulpa sinihil luptatur accuptas mo is vellabo rionectur samenih</a>
+			</div>
+			<div class="update-row">
+				<span class="update-date">10.24.16</span>
+				<a href="#">Imincip samusam re sinctorecab intur sum il</a>
+			</div>
+			<span id="see-more"><a href="https://nerds.inn.org/tag/project-largo/">Read More...</a></span>
 		</div>
 	</section>
 	<section>
