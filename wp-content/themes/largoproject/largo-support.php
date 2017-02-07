88851f10bc4af0f89640161383e5069acd5d7510
Gabriel Hongsdusit <gabriel@inn.org>
2017-02-07 13:45:23 -0800

    self-help styles

--- wp-content/themes/largoproject/largo-support.php
+++ wp-content/themes/largoproject/largo-support.php
@@ -7,31 +7,44 @@ $top_page = FALSE;
 
 ?>
  <div id="content" class="homepage-content clearfix span12">
- 	<section id="self-help">
+ 	<span id="page-title"><h2>Support</h2></span>
+ 	<section id="self-help" class="clearfix">
 	 	<div class="max-width-container">
 			<h5>Self-Help</h5>
-			<div>
-				<h2><a href="#">Knowledge Base</p></h2></a>
+			<div id="knowledge-base" class="clearfix">
+				<h2><a href="http://support.largoproject.org/support/solutions">Knowledge Base</p></h2></a>
 				<p>To beatur, sereicipis inulparum quatiis dollese volum et aut quo deliquae nem nim eius. Ti ipsa dustiaectem fugit.</p>
-				<a href="#">How Can I Improve the load time of my site?</a>
-				<a href="#">I lost my password. How can I find it?</a>
-				<a href="#">How can I make a post sticky?</a>
-				<a href="#">How can I add a link to a menu?</a>
+				<div class="row-fluid">
+					<div class="span2">
+						<a href="http://support.largoproject.org/support/solutions/articles/14000024125-how-can-i-improve-the-load-time-of-my-site-">How can I improve the load time of my site?</a>
+					</div>
+					<div class="span2">
+						<a href="http://support.largoproject.org/support/solutions/articles/14000019997-i-lost-my-password-how-can-i-find-it-">I lost my password. How can I find it?</a>
+					</div>
+					<div class="span2">
+						<a href="http://support.largoproject.org/support/solutions/articles/14000018948-how-can-i-make-a-post-sticky-">How can I make a post sticky?</a>
+					</div>
+					<div class="span2">
+						<a href="http://support.largoproject.org/support/solutions/articles/14000018942-how-can-i-add-a-link-to-a-menu-">How can I add a link to a menu?</a>
+					</div>
+				</div>
 			</div>
-			<div>
-				<h2>User Guides</h2>
-				<p>Isitatibus is magnis pro idebit, sundeles re moloreicae veliam res as estemque volumet remqui occusti ustias evendun.</p>
-			</div>
-			<div>
-				<a href="#" class="btn">For Developers</a>
-				<a href="#" class="btn">For Administrators</a>
-				<a href="#" class="btn">For Authors</a>
+			<div id="user-guides">
+				<div class="span5">
+					<h2>User Guides</h2>
+					<p>Isitatibus is magnis pro idebit, sundeles re moloreicae veliam res as estemque volumet remqui occusti ustias evendun.</p>
+				</div>
+				<div class="span5">
+					<a href="#" class="btn user-guide-btn" id="btn-developer">For Developers</a>
+					<a href="#" class="btn user-guide-btn" id="btn-admin">For Administrators</a>
+					<a href="#" class="btn user-guide-btn" id="btn-author">For Authors</a>
+				</div>
 			</div>
 		</div>
 	</section>
 	<section id="consulting-services">
 		<div class="max-width-container">
-			<h3>Consulting Services</h3>
+			<h5 id="support-header">Consulting Services</h5>
 			<h2>Office Hours</h2>
 			<p>Every Friday from 2-3 pm ET we hold open office hours via Zoom.us videocalls where anyone can come and talk to our team about projects or talk about general design, tech or product questions. You can signup for a 20 minute spot or just drop by. All are welcome!</p>
 			<a href="#">Sign Up for a Slot</a>
