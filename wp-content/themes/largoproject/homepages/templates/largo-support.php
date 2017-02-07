40144ffc54f373c0ff31aa755d6443470835a58b
Gabriel Hongsdusit <gabriel@inn.org>
2017-02-07 11:55:34 -0800

    adding support page template

--- wp-content/themes/largoproject/homepages/templates/largo-support.php
+++
@@ -1,2 +0,0 @@
-<h2>Support</h2>
-<p>This is the Largo Support page</p>
---
+++ wp-content/themes/largoproject/largo-support.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Template Name: Support Page
+ */
+get_header();
+$top_page = FALSE;
+
+?>
+ <div id="content" class="homepage-content clearfix span12">
+ 	<section id="self-help">
+	 	<div class="max-width-container">
+			<h5>Self-Help</h5>
+			<div>
+				<h2><a href="#">Knowledge Base</p></h2></a>
+				<p>To beatur, sereicipis inulparum quatiis dollese volum et aut quo deliquae nem nim eius. Ti ipsa dustiaectem fugit.</p>
+				<a href="#">How Can I Improve the load time of my site?</a>
+				<a href="#">I lost my password. How can I find it?</a>
+				<a href="#">How can I make a post sticky?</a>
+				<a href="#">How can I add a link to a menu?</a>
+			</div>
+			<div>
+				<h2>User Guides</h2>
+				<p>Isitatibus is magnis pro idebit, sundeles re moloreicae veliam res as estemque volumet remqui occusti ustias evendun.</p>
+			</div>
+			<div>
+				<a href="#" class="btn">For Developers</a>
+				<a href="#" class="btn">For Administrators</a>
+				<a href="#" class="btn">For Authors</a>
+			</div>
+		</div>
+	</section>
+	<section id="consulting-services">
+		<div class="max-width-container">
+			<h3>Consulting Services</h3>
+			<h2>Office Hours</h2>
+			<p>Every Friday from 2-3 pm ET we hold open office hours via Zoom.us videocalls where anyone can come and talk to our team about projects or talk about general design, tech or product questions. You can signup for a 20 minute spot or just drop by. All are welcome!</p>
+			<a href="#">Sign Up for a Slot</a>
+		</div>
+	</section>
+	<section id="largo-support" class="largo-section">
+		<div class="max-width-container">
+			<h2>Hire Us</h2>
+			<h4>We offer affordable consulting services (at an even more discounted rate for members). Our team can provide:</h4>
+			<div class="row-fluid">
+				<div class="span4">
+					<a href="#"><img class="icon frame-1" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/hire-us-01.svg" /></a>
+					<a href="https://inn.org/donate/"><img class="frame-2" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/hire-us-01B.svg" /></a>
+					<h5><a href="https://inn.org/donate/">Donate</a></h5>
+					<p>Support our work to improve nonprofit news</p>
+				</div>
+				<div class="span4">
+					<a href="#"><img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/hire-us-02.svg" /></a>
+					<a href="https://github.com/INN/docs/blob/master/how-to-work-with-us/contributing.md"><img class="frame-2" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/hire-us-02B.svg" /></a>
+					<h5><a href="https://github.com/INN/docs/blob/master/how-to-work-with-us/contributing.md">Translate</a></h5>
+					<p>Help us make Largo more accessible to the world</p>
+				</div>
+				<div class="span4">
+					<a href="#"><img class="icon" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/hire-us-03.svg" /></a>
+					<a href="https://github.com/INN/docs/blob/master/how-to-work-with-us/contributing.md"><img class="frame-2" src="<?php echo get_stylesheet_directory_uri(); ?>/homepages/assets/img/icons/hire-us-03B.svg" /></a>
+					<h5><a href="https://github.com/INN/docs/blob/master/how-to-work-with-us/contributing.md">Code</a></h5>
+					<p>hire-us your own ideas - we're listening</p>
+				</div>
+			</div>
+			<a href="#" class="btn">Learn More</a>
+		</div>
+	</section>
+	<section>
+		<div class="max-width-container">
+			<h3><a href="#">Help Desk</a></h3>
+			<p>Faceperis est, que volesto ex evel molupta nonsercidi bea sedi il ipsa dustiaectem fugit faceste nis eum atioreictia ist porepratatin.</p>
+			<a href="#">Submit a Support Ticket</a>
+		</div>
+	</section>
+</div>
+<?php get_footer(); ?>
