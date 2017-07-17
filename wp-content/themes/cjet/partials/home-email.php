98c0285907f00d1cfed3268b78968938be930326
Ben Keith <ben@benlk.com>
2017-07-17 14:50:04 -0400

    Rename and update the email template to match the wireframe

---
+++ wp-content/themes/cjet/partials/home-email.php
@@ -0,0 +1,17 @@
+<?php $img_path = get_stylesheet_directory_uri() . '/homepages/assets/img/'; ?>
+<section id="email" class="interstitial branding">
+	<div class="content">
+		<p class="">Get updates, INN member stories, training and more delivered straight to your inbox.</p>
+
+		<form id="mc-embedded-subscribe-form" class="validate" style="margin-bottom: 0;" action="//inn.us1.list-manage.com/subscribe/post?u=81670c9d1b5fbeba1c29f2865&amp;id=19bec3393e" method="post" name="mc-embedded-subscribe-form" novalidate="" target="_blank">
+			<label for="EMAIL" class="visuallyhidden">email address</label>
+			<input id="mce-EMAIL" class="email" name="EMAIL" required="" type="email" value="" placeholder="email address" />
+			<div style="position: absolute; left: -5000px;">
+				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				<input tabindex="-1" name="b_81670c9d1b5fbeba1c29f2865_19bec3393e" type="text" value="" />
+			</div>
+			<input id="mc-embedded-subscribe" class="btn button" name="subscribe" type="submit" value="Subscribe" />
+		</form>
+	</div>
+</section>
+
