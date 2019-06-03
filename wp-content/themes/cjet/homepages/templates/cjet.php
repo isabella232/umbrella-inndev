200bb5f7d19f92506b60166bbacc1cd6e6155a26
Ben Keith <ben@benlk.com>
2019-06-03 15:58:03 -0400

    Register new widget areas for homepage, reorder widgets for homepage, add styles to match mockup.

--- wp-content/themes/cjet/homepages/templates/cjet.php
+++ wp-content/themes/cjet/homepages/templates/cjet.php
@@ -1,70 +1,23 @@
-<?php
-
-$courses_parent = get_page_by_path('courses');
-$guides_parent = get_page_by_path('guides');
-
-$courses = get_pages( array(
-	'sort_column' => 'menu_order',
-	'parent' => $courses_parent->ID
-));
-
-$guides = get_pages( array(
-	'sort_column' => 'post_date',
-	'sort_order' => 'DESC',
-	'parent' => $guides_parent->ID
-));
-?>
-
-<section id="courses">
-	<h1><?php _e("Online Courses", 'cjet'); ?></h1>
-	<p class="description"><?php
-		if ( of_get_option('cjet_courses_intro') ) {
-			echo of_get_option('cjet_courses_intro');
-		} else {
-			_e('Edit this description under Appearance > Theme Options.', 'cjet');
-		}
-	?></p>
-	<ul>
-	<?php
-		foreach ( $courses as $course ) :	?>
-			<li>
-				<article>
-					<a href="<?php echo get_permalink( $course->ID ); ?>" title="Permalink to <?php echo esc_attr( $course->post_title ); ?>"><?php echo apply_filters('the_content', get_the_post_thumbnail( $course->ID, 'large' ) ); ?></a>
-					<h4><a href="<?php echo get_permalink( $course->ID ); ?>" title="Permalink to <?php echo esc_attr( $course->post_title ); ?>"><?php echo $course->post_title; ?></a></h4>
-					<p><?php echo $course->post_excerpt; ?></p>
-				</article>
-			</li>
-			<?php
-		endforeach;
-	?>
-	</ul>
-</section><!-- #courses -->
-
-<section id="extras">
-	<?php dynamic_sidebar( 'homepage-callout' ); ?>
+<section id="top">
+	<?php dynamic_sidebar( 'homepage-top' ); ?>
 </section>
 
 <section id="guides">
-	<h1><?php _e('Guides', 'cjet'); ?></h1>
+	<h2><?php _e('Explore guides', 'cjet'); ?></h2>
+
 	<p class="description"><?php
 		if ( of_get_option('cjet_guides_intro') ) {
 			echo of_get_option('cjet_guides_intro');
 		} else {
-			_e('Edit this description under Appearance > Theme Options.', 'cjet');
+			printf(
+				'<!-- %1$s -->',
+				_e('Edit this description under Appearance > Theme Options.', 'cjet')
+			);
 		}
 	?></p>
-	<ul>
-	<?php
-		foreach ( $guides as $guide ) : ?>
-			<li>
-				<article>
-					<a href="<?php echo get_permalink( $guide->ID ); ?>" title="Permalink to <?php echo esc_attr( $guide->post_title ); ?>"><?php echo apply_filters('the_content', get_the_post_thumbnail( $guide->ID, 'medium' ) ); ?></a>
-					<h4><a href="<?php echo get_permalink( $guide->ID ); ?>" title="Permalink to <?php echo esc_attr( $guide->post_title ); ?>"><?php echo $guide->post_title; ?></a></h4>
-					<p><?php echo $guide->post_excerpt; ?></p>
-				</article>
-			</li>
-			<?php
-		endforeach;
-	?>
-	</ul>
+
+
+	<div id="guides-container">
+		<?php dynamic_sidebar( 'homepage-guides' ); ?>
+	</div>
 </section><!-- #guides -->
