0a1e49018400c8c1a6b392ba496a6236f677c4ba
Ben Keith <ben@benlk.com>
2019-12-09 14:17:07 -0500

    Remove homepages path from Gruntfile

--- wp-content/themes/amplify/Gruntfile.js
+++ wp-content/themes/amplify/Gruntfile.js
@@ -1,25 +1,24 @@
 module.exports = function(grunt) {
     'use strict';
-  
+
     // Load all tasks
     require('load-grunt-tasks')(grunt);
     // Show elapsed time
     require('time-grunt')(grunt);
-  
+
     // Force use of Unix newlines
     grunt.util.linefeed = '\n';
-  
+
     // Find what the current theme's directory is, relative to the WordPress root
     var path = process.cwd().replace(/^[\s\S]+\/wp-content/, "\/wp-content");
-  
+
     var cssLessFiles = {
       'css/child-style.css': 'less/style.less',
-      'homepages/assets/css/homepage.css': 'homepages/assets/less/homepage.less'
     };
-  
+
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
-  
+
       less: {
         compile: {
           options: {
@@ -33,7 +32,7 @@ module.exports = function(grunt) {
           files: cssLessFiles
         }
       },
-  
+
       watch: {
         less: {
           files: [
@@ -46,7 +45,7 @@ module.exports = function(grunt) {
         }
       }
     });
-  
+
     // Build assets, docs and language files
     grunt.registerTask('default', 'Build less files', [
       'less',
