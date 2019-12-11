87b36b41ad0dcc50b3af35be1d2d061ce0d22242
Josh Darby <josh@inn.org>
2019-12-11 12:52:51 -0500

    Update embed feed to be dynamic by specified tag, also more link meta

--- wp-content/themes/amplify/amplify-feed.php
+++ wp-content/themes/amplify/amplify-feed.php
@@ -4,76 +4,110 @@
  * Template Name: Amplify Feed Template
  * Description: Shows the saved links feed that can be embedded on external sites.
  */
-
 ?>
 
-<img src="http://placehold.it/200x200">
-<h2>Lens on Lightfoot</h2>
-<a href="https://inn.org/amplify" target="_blank">Learn More</a>
+<div class="amplify-embed-feed">
 
-<?php
+    <?php
 
-$query_args = array (
-    'post__not_in' => get_option( 'sticky_posts' ),
-    'showposts'    => $instance['num_posts'],
-    'post_type'    => 'rounduplink',
-    'tax_query' => array(
-        array (
+    if( isset( $_GET['tag'] ) ){
+        
+        $term = $_GET['tag'];
+        $term = get_term_by( 'slug', $term, 'lr-tags' );
+
+        $term_more_link = get_term_link( $term, 'lr-tags' );
+        
+        if ( function_exists ( 'largo_get_term_meta_post' ) ) {
+            
+            $term_meta_post = largo_get_term_meta_post( 'lr-tags', $term->term_id );
+            $term_meta = get_post_meta( $term_meta_post );
+
+            if( $term_meta['lr_more_link'] ){
+                $term_more_link = $term_meta['lr_more_link'][0];
+            }
+
+            $thumbnail = get_the_post_thumbnail_url( $term->term_id, 'large' );
+            echo '<img src="'.$thumbnail.'">';
+
+        }
+        ?>
+        <h2><?php echo $term->name; ?></h2>
+        <p><?php echo $term->description; ?></p>
+        <a href="<?php echo $term_more_link; ?>" target="_blank">Learn More</a>
+        <?php
+    }
+
+    $query_args = array (
+        'post__not_in' => get_option( 'sticky_posts' ),
+        'showposts'    => $instance['num_posts'],
+        'post_type'    => 'rounduplink',
+        'post_status'  => 'publish'
+    );
+
+    if( $term ){
+
+        $tax_query = array (
             'taxonomy' => 'lr-tags',
             'field' => 'slug',
-            'terms' => 'lens-on-lightfoot',
-        )
-    ),
-    'post_status'  => 'publish'
-);
-$my_query = new WP_Query( $query_args );
+            'terms' => $term->slug,
+        );
 
-if ( $my_query->have_posts() ) {
+        $query_args['tax_query'] = array( $tax_query );
 
-    echo '<h3>Stories from the Project</h3>';
+    }
 
-    while ( $my_query->have_posts() ) : $my_query->the_post();
-    $saved_link = get_post_custom( $post->ID );
+    $my_query = new WP_Query( $query_args );
 
-    // skip roundups
-    if ( get_post_type( $post ) === 'roundup' ) continue; ?>
+    if ( $my_query->have_posts() ) {
 
-    <div class="saved-link clearfix">
-        <h5><?php
-            if ( isset( $saved_link["lr_url"][0] ) ) {
-                $output = '<a href="' . $saved_link["lr_url"][0] . '" ';
-                $output .= 'target="_blank" ';
-                $output .= '>' . get_the_title() . '</a>';
-            } else {
-                $output = get_the_title();
-            }
-            echo $output;
-            ?>
-        </h5>
+        echo '<h3>Stories from the Project</h3>';
 
-        <?php
-            if ( isset($saved_link["lr_source"][0] ) && !empty( $saved_link["lr_source"][0] ) ) {
-                $saved_link_source = '<p class="source">' . __('By: ', 'link-roundups') . '<span>';
-                if ( !empty( $saved_link["lr_url"][0] ) ) {
-                    $saved_link_source .= '<a href="' . $saved_link["lr_url"][0] . '" ';
-                    $saved_link_source .= 'target="_blank" ';
-                    $saved_link_source .= '>' . $saved_link["lr_source"][0] . '</a>';
+        while ( $my_query->have_posts() ) : $my_query->the_post();
+        $saved_link = get_post_custom( $post->ID );
+
+        // skip roundups
+        if ( get_post_type( $post ) === 'roundup' ) continue; ?>
+
+        <div class="saved-link clearfix">
+            <h5><?php
+                if ( isset( $saved_link["lr_url"][0] ) ) {
+                    $output = '<a href="' . $saved_link["lr_url"][0] . '" ';
+                    $output .= 'target="_blank" ';
+                    $output .= '>' . get_the_title() . '</a>';
                 } else {
-                    $saved_link_source .= $saved_link["lr_source"][0];
+                    $output = get_the_title();
                 }
-                $saved_link_source .= '</span></p>';
-                echo $saved_link_source;
-            }
-            if ( isset( $saved_link["lr_desc"][0] ) ) {
-                echo '<div class="description">';
-                    echo $saved_link["lr_desc"][0];
-                echo '</div>';
-            }
-        ?>
-    </div> <!-- /.post-lead -->
-    
-<?php
-    endwhile;
-} else {
-    _e( '<p class="error">You don\'t have any recent links or the link roundups plugin is not active.</p>', 'link-roundups' );
-} // end recent links
+                echo $output;
+                ?>
+            </h5>
+
+            <?php
+                if ( isset($saved_link["lr_source"][0] ) && !empty( $saved_link["lr_source"][0] ) ) {
+                    $saved_link_source = '<p class="source">' . __('By: ', 'link-roundups') . '<span>';
+                    if ( !empty( $saved_link["lr_url"][0] ) ) {
+                        $saved_link_source .= '<a href="' . $saved_link["lr_url"][0] . '" ';
+                        $saved_link_source .= 'target="_blank" ';
+                        $saved_link_source .= '>' . $saved_link["lr_source"][0] . '</a>';
+                    } else {
+                        $saved_link_source .= $saved_link["lr_source"][0];
+                    }
+                    $saved_link_source .= '</span></p>';
+                    echo $saved_link_source;
+                }
+                if ( isset( $saved_link["lr_desc"][0] ) ) {
+                    echo '<div class="description">';
+                        echo $saved_link["lr_desc"][0];
+                    echo '</div>';
+                }
+            ?>
+        </div>
+        
+    <?php
+        endwhile;
+    } else {
+        _e( '<p class="error">You don\'t have any recent links or the link roundups plugin is not active.</p>', 'link-roundups' );
+    } // end recent links
+
+    ?>
+
+</div>
--- wp-content/themes/amplify/functions.php
+++ wp-content/themes/amplify/functions.php
@@ -84,3 +84,52 @@ function amplify_load_feed_template( $template ) {
 	
 }
 add_filter( 'template_include', 'amplify_load_feed_template' );
+
+/**
+ * Add the "More link" input field to the tag edit page for 'lr-tags' taxonomy
+ * 
+ * @param obj $context The wp term that is being modified
+ */
+function amplify_add_saved_link_roundups_more_link_metabox( $context = '' ) {
+
+	if( 'lr-tags' == $context->taxonomy ){
+		
+		$term_more_link = get_term_link( $context, $context->taxonomy );
+
+		// Post ID here is the id of the post that Largo uses to keep track of the term's metadata. See largo_get_term_meta_post.
+		$post_id = largo_get_term_meta_post( $context->taxonomy, $context->term_id );
+
+		if( metadata_exists( 'post', $post_id, 'lr_more_link' ) ) {
+			$term_more_link = get_post_meta( $post_id, 'lr_more_link', true );
+		}
+
+		?>
+		<tr class="form-field">
+			<th scope="row" valign="top"><?php _e('More link', 'amplify'); ?></th>
+			<td>
+				<input class="widefat" id="more_link" name="lr_more_link" type="url" value="<?php echo $term_more_link; ?>" />
+				<p class="description">If the "More" link that is displayed needs to redirect to somewhere other than the tag archive page, change it here.</p>
+			</td>
+		</tr>
+		<?php
+
+	}
+
+}
+add_action( 'edit_tag_form_fields', 'amplify_add_saved_link_roundups_more_link_metabox');
+
+/**
+ * Function to save specific meta for LR tags	
+ */
+function save_lr_tag_meta( $term_id ) {
+
+	// we'll need to get the post id of the term that Largo uses to keep track of its meta. See largo_get_term_meta_post.
+	$post_id = largo_get_term_meta_post( 'lr-tags', $term_id );
+
+	// save the "More" link meta if posible
+	if( isset( $_POST['lr_more_link'] ) && ! empty( $_POST['lr_more_link'] ) ) {
+		update_post_meta( $post_id, 'lr_more_link', sanitize_url( $_POST['lr_more_link'] ) );
+	}
+
+}
+add_action( 'edit_term', 'save_lr_tag_meta' );
