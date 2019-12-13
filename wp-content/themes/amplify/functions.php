2bcf29791ca363de5eef74a09e4e80bf7471dd2f
Josh Darby <josh@inn.org>
2019-12-13 12:43:07 -0500

    Add support for typekit and update body font

--- wp-content/themes/amplify/functions.php
+++ wp-content/themes/amplify/functions.php
@@ -38,6 +38,12 @@ function largo_parent_theme_enqueue_styles() {
 	wp_dequeue_style( 'largo-child-styles' );
 
 	wp_enqueue_style( 'largo-style', get_template_directory_uri() . '/style.css' );
+
+	wp_enqueue_style(
+		'typekit',
+		'https://use.typekit.net/ppf3kvq.css'
+	);
+
 	wp_enqueue_style( 'amplify-style',
 		get_stylesheet_directory_uri() . '/css/child-style.css',
 		array( 'largo-stylesheet' ),
